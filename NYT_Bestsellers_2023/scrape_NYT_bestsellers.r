140ed791a4f66d82734606df1c9ecc9000333149
Lekha Mirjankar <lekhamirjankar@gmail.com>
2024-01-23 01:29:55 +0530

    extracted genre information

--- NYT_Bestsellers_2023/scrape_NYT_bestsellers.r
+++ NYT_Bestsellers_2023/scrape_NYT_bestsellers.r
@@ -63,7 +63,76 @@ combined_data <- do.call(rbind, all_data)
 #combined_data
 
 # Specify the file path for the Excel file
-excel_file_path <- "nyt_bestsellers.xlsx"
+#excel_file_path <- "nyt_bestsellers.xlsx"
 
 # Write the data frame to an Excel file
-write.xlsx(combined_data, file = excel_file_path, sheetName = "Bestsellers 2023", rowNames = TRUE)
+#write.xlsx(combined_data, file = excel_file_path, sheetName = "Bestsellers 2023", rowNames = TRUE)
+
+# Function to scrape genre information from goodreads
+scrapeGenres <- function(title) {
+  title <- gsub(" ", "%20", title, fixed = TRUE)
+  
+  # Construct the url
+  search_query <- paste0(title)
+  
+ search_url <- paste0("https://www.goodreads.com/search?q=", URLencode(search_query))
+  
+  # Perform google search to get the goodreads link
+  search_results <- read_html(search_url, encoding = "latin1")
+  
+  # Extract the link to the first result
+  first_result_link <- search_results %>%
+    html_nodes("a.bookTitle") %>%
+    html_attr("href") %>%
+    head(1)
+  
+  # Construct the full URL for the first result
+  full_url <- paste0("https://www.goodreads.com", first_result_link)
+  
+  # Scrape the genre information
+  genres <- full_url %>%  
+    read_html() %>%  
+    html_nodes(".BookPageMetadataSection__genres a") %>%  
+    html_text()
+ 
+  return(genres)
+}
+
+# Test if the function is working 
+# Extract the first title from the NYT dataframe
+# first_title <- combined_data$Title[1]
+# Run the script for the first title
+# genres <- scrapeGenres(first_title)
+# Print the genres
+# print(genres)
+
+# Iterating through titles in the NYT dataframe
+combined_data_with_genres <- combined_data
+combined_data_with_genres$Genre1 <- NA
+combined_data_with_genres$Genre2 <- NA
+combined_data_with_genres$Genre3 <- NA
+combined_data_with_genres$Genre4 <- NA
+
+for (i in seq_len(nrow(combined_data_with_genres))) {
+  title <- combined_data_with_genres$Title[i]
+  genres <- scrapeGenres(title)
+  
+  # Assign top 4 genres
+  combined_data_with_genres$Genre1[i] <- genres[1]
+  combined_data_with_genres$Genre2[i] <- genres[2]
+  combined_data_with_genres$Genre3[i] <- genres[3]
+  combined_data_with_genres$Genre4[i] <- genres[4]
+  
+  # Delay to avoid overwhelming the site
+  Sys.sleep(10)
+}
+
+# Create a final genre column based on conditions
+NYT_df_combined_genre <- combined_data_with_genres %>% 
+  mutate(Genre = case_when(
+    !(Genre1 %in% c('Fiction', 'Audiobook', 'Contemporary')) ~ Genre1,
+    !(Genre2 %in% c('Fiction', 'Audiobook', 'Contemporary')) ~ Genre2,
+    !(Genre3 %in% c('Fiction', 'Audiobook', 'Contemporary')) ~ Genre3,
+    !(Genre4 %in% c('Fiction', 'Audiobook', 'Contemporary')) ~ Genre4,
+    TRUE ~ 'Unknown'
+  ))
